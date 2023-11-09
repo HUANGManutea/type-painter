7f7e171a078eda077c230e93d011609ea337b539
huang <huang.manutea@gmail.com>
2023-11-09 13:18:40 -1000

    fixed infos

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -19,7 +19,7 @@ export default function Home() {
         <div className='flex flex-col gap-2'>
           <h2>Informations:</h2>
           <p>This is a toy project, made after seeing a post on Facebook about a typewriter that was modified to paint instead of writing letters. (<Link href={"https://tyreecallahan.blogspot.com/2011/12/introducing-chromatic-typewriter-2012.html?m=1"}>original blogpost</Link>, artist: Tyree Callahan)</p>
-          <p>This website and the author do not have any affiliation with the Facebook post or the original typewriter.</p>
+          <p>This website and the author do not have any affiliation with the Facebook post, the blogpost, or the original typewriter.</p>
           <p>The keyboard events are filtered to draw on the canvas, the accepted keys are only the letters (no special character) and the arrow keys.</p>
           <p>In the original palette, I tried to reproduce the colors on the original typewriter, but as I have reduced the number of keys, it is not an exact match.</p>
           <p>The source code of this website can be found <Link href={"https://github.com/HUANGManutea/type-painter"}>here</Link>.</p>
